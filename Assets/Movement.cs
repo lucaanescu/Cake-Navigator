e230f69cf5b7d5a2e6e9d3428c40fc3b81fa6289
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2022-07-25 15:47:22 -0400

    Some camera and control changes. It needs more work before it's finished

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController controller;
 
-    // Update is called once per frame
+    public float speed = 5f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
     void Update()
     {
-        
+       float horizontal = Input.GetAxisRaw("Horizontal");
+       float vertical = Input.GetAxisRaw("Vertical");
+       Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+       if (direction.magnitude >= 0.1f)
+       {
+           float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+           float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+           transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+           controller.Move(direction * speed * Time.deltaTime);
+       }
     }
 }
