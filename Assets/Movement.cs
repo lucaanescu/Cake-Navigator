0e8b83ec41c3d7f0b209b72d8ba4ae33e4358fca
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2022-07-26 17:21:05 -0400

    More movement and camera changes
    Because having the camera following nzomi gave me such a headache, im just going to have the game be top-down and have two cameras that activate and de-activate based on threshhold passed

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,39 +5,36 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 	public Rigidbody rb;
+	private float speed = 12;
 	private Vector3 input;
-
-	private float speed = 6f;
 
 	void Update()
 	{
 		GatherInput();
-		LookInDirection();
+		Look();
 	}
 
-	void FixedUpdate(){
+	void FixedUpdate()
+	{
 		Move();
 	}
 
-	void GatherInput(){
-		input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+	void GatherInput()
+	{
+		input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
 	}
 
-	void LookInDirection(){
-		if(input != Vector3.zero)
-		{
-
-		
+	void Look()
+	{
+		if (input != Vector3.zero){
 			var relative = (transform.position + input) - transform.position;
-			var rot = Quaternion.LookRotation(relative,Vector3.up);
+			var rot = Quaternion.LookRotation(relative, Vector3.up);
 
 			transform.rotation = rot;
 		}
 	}
 
 	void Move(){
-
 		rb.MovePosition(transform.position + (transform.forward * input.magnitude) * speed * Time.deltaTime);
 	}
-
 }
