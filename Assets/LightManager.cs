4f7448ff5f508401dc80a596888c9e700d85ba64
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2022-07-29 12:02:35 -0400

    Trying to add new skyboxes and timed transitions

--- Assets/LightManager.cs
+++ Assets/LightManager.cs
@@ -7,8 +7,30 @@ public class LightManager : MonoBehaviour
 	public Material Sky1;
 	public Material Sky2;
 
+	public bool sky1 = true;
+	public bool sky2 = false;
+
 	void Awake()
 	{
 		RenderSettings.skybox = Sky1;
 	}
+
+	public void Update()
+	{
+		if(Input.GetButtonDown("Switch") && sky2 == false)
+		{
+			sky2 = true;
+			sky1 = false;
+			RenderSettings.skybox = Sky2;
+			
+		}
+		else if(Input.GetButtonDown("Switch") && sky2 == true)
+		{
+			sky2 = false;
+			sky1 = true;
+			RenderSettings.skybox = Sky1;
+			
+		}
+	}
+
 }
