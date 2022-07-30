50a98e0e6782abd968ff222b4ab08bb00f6c122d
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2022-07-30 16:51:59 -0400

    Adjusted timer
    Timer slows down and then readjusts after one button press, all thats left to do now is add fade ins and outs

--- Assets/LightManager.cs
+++ Assets/LightManager.cs
@@ -7,8 +7,15 @@ public class LightManager : MonoBehaviour
 	public Material Sky1;
 	public Material Sky2;
 
+	public Light light1;
+	public Light light2;
+
 	public bool sky1 = true;
 	public bool sky2 = false;
+
+	private float timer = 0.2f;
+	private float timeleft = 0.2f;
+	private bool TimerOn = false;
 
 	void Awake()
 	{
@@ -17,20 +24,43 @@ public class LightManager : MonoBehaviour
 
 	public void Update()
 	{
-		if(Input.GetButtonDown("Switch") && sky2 == false)
+
+		Debug.Log(timeleft);
+
+		if(Input.GetButtonDown("Switch"))
 		{
-			sky2 = true;
-			sky1 = false;
-			RenderSettings.skybox = Sky2;
-			
+			TimerOn = true;
 		}
-		else if(Input.GetButtonDown("Switch") && sky2 == true)
+
+		if(TimerOn == true && timeleft > 0f)
+			{
+				timeleft -= Time.deltaTime;
+				Time.timeScale = 0.1f;
+				if(timeleft < 0f && sky2 == false)
+				{
+					sky2 = true;
+					light1.enabled = true;
+					light2.enabled = false;
+					sky1 = false;
+					RenderSettings.skybox = Sky2;
+				}
+				else if(timeleft < 0 && sky2 == true)
+				{
+					sky2 = true;
+					light1.enabled = false;
+					light2.enabled = true;
+					sky1 = false;
+					RenderSettings.skybox = Sky1;
+				}
+			}
+		if(timeleft <= 0f)
 		{
-			sky2 = false;
-			sky1 = true;
-			RenderSettings.skybox = Sky1;
-			
+			timeleft = timer;
+			TimerOn = false;
+			Time.timeScale = 1f;
+			Debug.Log("I made it");
 		}
+
 	}
 
 }
