af975dc20caee2f3ee66a7aa9824f69dd4834779
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2022-07-30 21:00:47 -0400

    Transition finished
    Not Exactly what I had in mind but it works

--- Assets/LightManager.cs
+++ Assets/LightManager.cs
@@ -13,13 +13,16 @@ public class LightManager : MonoBehaviour
 	public bool sky1 = true;
 	public bool sky2 = false;
 
-	private float timer = 0.2f;
-	private float timeleft = 0.2f;
+	private float timer = 0.02f;
+	private float timeleft = 0.02f;
 	private bool TimerOn = false;
+
+	[SerializeField] public CanvasGroup Fade;
 
 	void Awake()
 	{
 		RenderSettings.skybox = Sky1;
+		Fade.alpha = 0f;
 	}
 
 	public void Update()
@@ -35,22 +38,27 @@ public class LightManager : MonoBehaviour
 		if(TimerOn == true && timeleft > 0f)
 			{
 				timeleft -= Time.deltaTime;
-				Time.timeScale = 0.1f;
-				if(timeleft < 0f && sky2 == false)
+				Time.timeScale = 0.01f;
+				Fade.alpha += Time.deltaTime;
+
+				if(timeleft < 0.01f && sky2 == false)
 				{
+					
 					sky2 = true;
 					light1.enabled = true;
 					light2.enabled = false;
 					sky1 = false;
 					RenderSettings.skybox = Sky2;
+
 				}
-				else if(timeleft < 0 && sky2 == true)
+				else if(timeleft < 0.01f && sky2 == true)
 				{
-					sky2 = true;
+					sky2 = false;
 					light1.enabled = false;
 					light2.enabled = true;
-					sky1 = false;
+					sky1 = true;
 					RenderSettings.skybox = Sky1;
+
 				}
 			}
 		if(timeleft <= 0f)
@@ -58,7 +66,7 @@ public class LightManager : MonoBehaviour
 			timeleft = timer;
 			TimerOn = false;
 			Time.timeScale = 1f;
-			Debug.Log("I made it");
+			Fade.alpha = 0f;
 		}
 
 	}
